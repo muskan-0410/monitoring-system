610b9ce05b3fbb37b1590e5b984732e82a231602
muskan-0410 <yadavmuskan844@gmail.com>
2026-03-20 16:49:18 +0530

    generate fake alerts with random cpu

--- backend/websocket/wsServer.js
+++ backend/websocket/wsServer.js
@@ -8,6 +8,16 @@ wss.on("connection", (ws) => {
   ws.send(JSON.stringify({
     message: "Connected to monitoring system"
   }));
+
+  setInterval(() =>{
+    const alert = {
+        type: "CPU_ALERT",
+        value: Math.floor(Math.random()*100),
+        message: "CPU usage high"
+    };
+
+    ws.send(JSON.stringify(alert));
+  }, 5000);
 });
 
 module.exports = wss;
