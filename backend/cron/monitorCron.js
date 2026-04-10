5813374f17db3a41ee586ef377d3a544d1b243a4
muskan-0410 <yadavmuskan844@gmail.com>
2026-04-10 15:23:41 +0530

    add memory and disk alert in frontend

--- backend/cron/monitorCron.js
+++ backend/cron/monitorCron.js
@@ -9,18 +9,36 @@ cron.schedule("*/10 * * * * *", () => {
   console.log("Running cron job...");
 
   getSystemMetrics((data) => {
+    if (data.cpu > config.cpuThreshold) {
+      alertEmitter.emit("thresholdBreach", {
+        type: "CPU_ALERT",
+        value: data.cpu,
+        message: "High CPU usage"
+      });
+    }
 
-  const isHigh = data.cpu > config.cpuThreshold;
+    if (data.memory > 80) {
+      alertEmitter.emit("thresholdBreach", {
+        type: "MEMORY_ALERT",
+        value: data.memory,
+        message: "High Memory usage"
+      });
+    }
 
-  const alert = {
-    type: "CPU_ALERT",
-    value: data.cpu,
-    memory: data.memory,
-    disk: data.disk,
-    message: isHigh ? "High CPU usage" : "Normal"
-  };
+    if (data.disk > 80) {
+      alertEmitter.emit("thresholdBreach", {
+        type: "DISK_ALERT",
+        value: data.disk,
+        message: "High Disk usage"
+      });
+    }
 
-  alertEmitter.emit("thresholdBreach", alert);
+    alertEmitter.emit("thresholdBreach", {
+      type: "SYSTEM_METRICS",
+      cpu: data.cpu,
+      memory: data.memory,
+      disk: data.disk
+    });
 
   });
 });
